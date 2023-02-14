13efa644d528e7850e29072528c8ec11fe73fd86
Ana Júlia <meloanajulia30@gmail.com>
2023-02-14 17:21:40 -0300

    site completo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-let menu = document.querySelector('.menu-icon');
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('move');
+    navbar.classList.toggle("open-menu");
+    menu.classList.toggle("move");
+};
+window.onscroll = () => {
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move");
 }
-
 // reviews Swiper
 var swiper = new Swiper(".reviews-content", {
     spaceBetween: 30,
@@ -17,3 +22,58 @@ var swiper = new Swiper(".reviews-content", {
         clickable: true,
     },
     });
+
+//Email Js
+function validate() {
+    let name = document.querySelector('.name')
+    let email = document.querySelector('.email')
+    let msg = document.querySelector('.message')
+    let sendBtn = document.querySelector('.send-btn')
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == ""){
+        emptyerror();
+        } else {
+            sendemail(name.value, email.value, msg.value);
+            success();
+        }
+    })
+}    
+validate();
+
+function sendmail(name, email, msg) {
+    emailjs.send("service_gtyaifm","template_4ui0bzh",{
+        from_name: email,
+        to_name: name,
+        message: msg,
+        });        
+}
+
+function emptyerror() {
+    swal({
+        title: "Oh No....",
+        text: "Os campos não podem estar vazios!",
+        icon: "error",
+      });
+}
+function success() {
+    swal({
+        title: "Email enviado com Sucesso",
+        text: "Tentaremos responder em 24 horas!",
+        icon: "success",
+      });
+}
+// Header background Change On Scrool
+let header = document.querySelector("header")
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("header-active", window.scroolY > 0);
+});
+
+// Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    scrollTop.classList.toggle(".scroll-active", window.scroolY >= 400);
+});
